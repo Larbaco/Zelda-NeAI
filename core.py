d49c4b7da375deafffcc2e79a9a04704ad3285e9
Thiago Cabral <thiagoo.cabral@gmail.com>
2019-04-25 10:54:07 -0300

    Movimentacao e Colisao
    implementando movimentação do personagem e começando a trabalhar na malha de colisão

--- core.py
+++ core.py
@@ -23,12 +23,20 @@ class Zenai(arcade.Window):
         arcade.set_background_color((155, 192, 122))  # FFC07A
         self.x = 7
         self.y = 7
+        self.pos_x_personagem = 88
+        self.pos_y_personagem = 128
+        self.down = 0
+        self.up = 0
+        self.left = 0
+        self.right = 0
 
     def setup(self):
         """ Set up the game and initialize the variables. """
 
         # Create the sprite lists
-
+        self.player = arcade.Sprite('sprites/character.png', multiplier)
+        self.player.set_position(self.pos_y_personagem, self.pos_x_personagem)
+        self.player.draw()
         # Score
 
         global world
@@ -39,40 +47,74 @@ class Zenai(arcade.Window):
                 a += world[i][j] + '| '
             # print(a)
         # print()
+        self.colisionmap = map.gera_colisoes()
 
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
-        print("quadrante x,y ", self.x, self.y)
+        # print("quadrante x,y ", self.x, self.y)
         map.draw_room(world, (self.x, self.y))
 
         if self.last_time and self.frame_count % 60 == 0:
             fps = 1.0 / (time.time() - self.last_time) * 60
             self.fps_message = f"FPS: {fps:5.0f}"
 
+        self.player.set_position(self.pos_y_personagem, self.pos_x_personagem)
+        self.player.draw()
+
     def update(self, delta_time):
         """ All the logic to move, and the game logic goes here. """
-        pass
+        colisao = map.getColisao(self.pos_x_personagem + 1, self.pos_y_personagem + 1, self.x, self.y, self.colisionmap)
+        if self.left:
+            if self.pos_y_personagem > 0:
+                if colisao:
+                    pass
+                self.pos_y_personagem -= 2;
+            else:
+                self.pos_y_personagem = 249
+                self.y -= 1;
+                pass
+        elif self.right:
+            if self.pos_y_personagem < 250:
+                self.pos_y_personagem += 2;
+            else:
+                self.pos_y_personagem = 0
+                self.y += 1
+                pass
+        elif self.down:
+            if self.pos_x_personagem > 0:
+                self.pos_x_personagem -= 2;
+
+            else:
+                self.pos_x_personagem = 165
+                self.x += 1
+                pass
+        elif self.up:
+            if self.pos_x_personagem < 170:
+                self.pos_x_personagem += 2;
+            else:
+                self.pos_x_personagem = 0
+                self.x -= 1
+                pass
 
     def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
         if key == arcade.key.LEFT:
-            if self.y > 0:
-                self.y -= 1;
-            else:
-                pass
+            self.left = 1
         elif key == arcade.key.RIGHT:
-            if self.y < 15:
-                self.y += 1;
-            else:
-                pass
-        elif key == arcade.key.UP:
-            if self.x > 0:
-                self.x -= 1;
-            else:
-                pass
+            self.right = 1
         elif key == arcade.key.DOWN:
-            if self.x < 7:
-                self.x += 1;
-            else:
-                pass
+            self.down = 1
+        elif key == arcade.key.UP:
+            self.up = 1
+
+    def on_key_release(self, key, modifiers):
+        """ Called whenever the user presses a key. """
+        if key == arcade.key.LEFT:
+            self.left = 0
+        elif key == arcade.key.RIGHT:
+            self.right = 0
+        elif key == arcade.key.DOWN:
+            self.down = 0
+        elif key == arcade.key.UP:
+            self.up = 0
--- map.py
+++ map.py
@@ -14,16 +14,29 @@ def create_map():
 
 def draw_room(world_map, pos):
     sprite_tiles = arcade.SpriteList()
-    debug = ''
     for linha in range(11):
         for coluna in range(16):
-            # print(world_y, world_x)
-            tile_linha = (pos[0]*11)+linha
-            tile_coluna= (pos[1]*16)+coluna
-            tile = arcade.Sprite('sprites/' + world_map[tile_linha][tile_coluna] + '.png',multiplier)
-            tile.set_position((multiplier*(coluna*16)+8),(multiplier*(167-(linha*16))))
-            #tile.center_x = ((coluna * 16) + 8)
-            #tile.center_y = ((linha * 16) + 8)
+            tile_linha = (pos[0] * 11) + linha
+            tile_coluna = (pos[1] * 16) + coluna
+            tile = arcade.Sprite('sprites/' + world_map[tile_linha][tile_coluna] + '.png', multiplier)
+            tile.set_position((multiplier * (coluna * 16) + 8), (multiplier * (167 - (linha * 16))))
             sprite_tiles.append(tile)
-            # debug += world_map[pos[0] + x][pos[1] + y] + ' '
     sprite_tiles.draw()
+
+
+def gera_colisoes():
+    mapa_colisoes = []
+    file = open('Colisoes.txt')
+    for linha in file:
+        mapa_colisoes += [linha.split()]
+    print(mapa_colisoes)
+    return mapa_colisoes
+
+
+def getColisao(pos_x_personagem, pos_y_personagem, room_x, room_y, colisionmap):
+    coluna = (pos_y_personagem / 16)
+    linha = (11 - (pos_x_personagem / 11))
+    linha = int(linha)
+    coluna = int(coluna)
+    print(linha, coluna, room_x, room_y)
+    #print(colisionmap[linha])
